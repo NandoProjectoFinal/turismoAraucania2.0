20057270d0ccecc13c9c7b1e70b55f55f4a93b51
Nawel <n.huenchuleo01@ufromail.cl>
2021-07-14 16:01:35 -0400

    turismo

---
+++ src/main/java/Lugares.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Lugares {
+    private String Nombre;
+    private String Ubicacion;
+
+    public Lugares(String nombre, String ubicacion) {
+        Nombre = nombre;
+        Ubicacion = ubicacion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        Ubicacion = ubicacion;
+    }
+
+    public static String[] listanombres(){
+        GestorArchivo a = new GestorArchivo();
+        String ruta = "D:\\Proyectos\\turismoAraucania\\Lugares\\Lugares\\";
+        String[] lista = a.leerArchivo(ruta).split("\n");
+        return lista;
+    }
+    public static String[] listadatos(){
+        GestorArchivo a = new GestorArchivo();
+        String ruta = "D:\\Proyectos\\turismoAraucania\\Lugares\\Localización\\";
+        String[] datos = a.leerArchivo(ruta).split("\n");
+        return datos;
+    }
+    public static void Lugares(){
+        System.out.println("Aquí está la lista de datos sobre los lugares turísticos / " + " Here is the list of facts about tourist places");
+        String[] listaNombre = listanombres();
+        for (int i=0; i<listaNombre.length; i++){
+            System.out.println("["+(i)+"]" + " " + listaNombre[i]);
+        }
+    }
+    public static int seleccion() {
+        Scanner teclado = new Scanner(System.in);
+        int respuesta;
+        do {
+            System.out.println("¿Qué lugar desea ver?/ What place do you want to see?");
+            respuesta = teclado.nextInt();
+        } while (respuesta > 8 || respuesta<0 );
+        return respuesta;
+    }
+}
